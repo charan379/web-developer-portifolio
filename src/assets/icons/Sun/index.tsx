fda1aed70d597160aa57c3064e5cc124807c0656
Charanteja <37008444+charan379@users.noreply.github.com>
2023-07-07 12:30:27 +0000

    increase icons size to 30x30

--- src/assets/icons/Sun/index.tsx
+++ src/assets/icons/Sun/index.tsx
@@ -7,8 +7,8 @@ export const SunSvgIcon = () => {
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
-                width="20"
-                height="20"
+                width="30"
+                height="30"
             >
                 <path fill="none" d="M0 0h24v24H0z" />
                 <path
