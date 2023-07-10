ba573b232fdd6b03573f8480f05a8b58f2935880
Charanteja <37008444+charan379@users.noreply.github.com>
2023-07-10 11:54:38 +0000

    added about intro

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const About  = () => {
+const About = () => {
     return (
         <>
             <section id="about">
@@ -11,18 +11,16 @@ const About  = () => {
                         </header>
                         <div className="content">
                             <p>
-                                Me chamo Lucyan Ovídio, graduando no curso de Sistemas de
-                                Informação pela UFRRJ e desenvolvedor web Front-end. Desenvolvo páginas e sistemas web responsívos e de layout atrativo. Tenho uma trajetória de cerca de 4 anos
-                                trabalhando com suporte técnico e manutenção de computadores, o
-                                que me tornou um profissional dedicado e organizado. Hoje na área de desenvolvimento vivo o aprendizado continuo, focando em tecnologias JavaScript. Busco oportunidades no mercado como dev e, em um futuro próximo, viso me tornar um desenvolvedor Full Stack.
+                                My name is Charan Teja, graduated in Bsc.(Computer Science). I develop responsive web pages and systems with an attractive layout. I have a trajectory of about 2 years
+                                working with technical support and maintenance of computers, that made me a dedicated and organized professional. Today in the area of ​​live development, continuous learning is focused on JavaScript technologies. I'm looking for opportunities in the market as a dev and, in the near future, I aim to become a Full Stack developer.
                             </p>
                         </div>
                     </div>
 
                     <div className="col-b">
                         <img
-                            src="./assets/images/imagem-de-lucyan-vestindo-camisa-social-branca-e-sorrindo-com-plantas-no-fundo.png"
-                            alt="Imagem de Lucyan vestindo camisa social branca e sorrindo com plantas no fundo"
+                            src="./assets/images/imagem-de-lucyan-vestindo-camisa-social-branca-e-sorrindo-com-plantas-no-fundo.pn"
+                            alt="Profile image"
                         />
                     </div>
                 </div>
