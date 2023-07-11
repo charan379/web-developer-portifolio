00c9c65c61efe3d6464d4413a7ba793ffc403b8e
Charanteja <37008444+charan379@users.noreply.github.com>
2023-07-11 10:58:47 +0000

    skill type updated

--- src/components/Skills/SkillCard/index.tsx
+++ src/components/Skills/SkillCard/index.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const SkillCard = (props: Skill) => {
     return (
         <div className="card">
-            <img src={props.src} alt={props.alt} />
+            <img src={props.image} alt={`${props.name} Icon`} />
 
             <h3>{props.name}</h3>
             <p>
--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -4,24 +4,21 @@ import SkillCard from './SkillCard'
 
 const Skills = () => {
 
-    const skills: Skill[] = [
+    const demoSkills: Skill[] = [
         {
             name: "HTML5",
             description: "HTML5 is a hypertext markup language, which we use totag the elements of the page and build it.",
-            src: "./assets/images/html.png",
-            alt: "HTML5 Icon."
+            image: "./assets/images/html.png",
         },
         {
             name: "CSS3",
             description: "CSS3 is a style language used to describe the presentation of page elements by styling them.",
-            src: "./assets/images/css.png",
-            alt: "CSS3 Icon."
+            image: "./assets/images/css.png",
         },
         {
             name: "JavaScript",
             description: "JavaScript is an interpreted programming language used in web development to describe features and page behaviors.",
-            src: "./assets/images/js.png",
-            alt: "Javascript Icon."
+            image: "./assets/images/js.png",
         },
     ]
     return (
@@ -33,8 +30,8 @@ const Skills = () => {
                     </header>
                     <div className="content">
                         <div className="cards">
-                            {skills.map((skill, index) => {
-                                return (<SkillCard src={skill.src} alt={skill.alt} name={skill.name} description={skill.description} key={index} />)
+                            {demoSkills.map((skill, index) => {
+                                return (<SkillCard image={skill.image} name={skill.name} description={skill.description} key={index} />)
                             })
                             }
                         </div>
