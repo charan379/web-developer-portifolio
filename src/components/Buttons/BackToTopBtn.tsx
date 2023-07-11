da91ca755a74fa2fa1079de29b656978a93f6d6b
Charanteja <37008444+charan379@users.noreply.github.com>
2023-07-11 10:57:03 +0000

    modularize svg icon

--- src/components/Buttons/BackToTopBtn.tsx
+++ src/components/Buttons/BackToTopBtn.tsx
@@ -1,32 +1,11 @@
+import { UpArrowSvgIcon } from '@/assets/icons/UpArrow'
 import React from 'react'
 
 const BackToTopBtn = () => {
     return (
         <>
-            <a id="backToTopButton" title="Voltar ao topo" href="#home">
-                <svg
-                    width="40"
-                    height="40"
-                    viewBox="0 0 40 40"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <circle cx="20" cy="20" r="20" fill="#00856F" />
-                    <path
-                        d="M20 27V13"
-                        stroke="white"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                    />
-                    <path
-                        d="M13 20L20 13L27 20"
-                        stroke="white"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                    />
-                </svg>
+            <a id="backToTopButton" title="Back to the top" href="#home">
+                <UpArrowSvgIcon />
             </a>
         </>
     )
