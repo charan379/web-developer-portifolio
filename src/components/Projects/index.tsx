448c4b6aa0232a0592e4c95441e838f7b8744e46
Charanteja <37008444+charan379@users.noreply.github.com>
2023-07-11 10:57:23 +0000

    projects section

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import ProjectCard from './ProjectCard'
 
 const Projects = () => {
+
+    const demoProjects: Array<Project> = [
+        {
+            name: 'Orange Evolution',
+            description: 'Study platform developed in the Hackathon of the FCamara Training Program 4.',
+            image: './assets/images/projects/orange-evolution.gif',
+            technologies: ["HTML", "CSS", "JS", "NodeJS", "Express", "SQLite"],
+            github: 'https://github.com/squad-30/OrangeEvolution-Squad30',
+            demo: 'https://orangeevolution-squad30.up.railway.app'
+        }
+    ]
     return (
         <>
             <section id="projects">
-                <div className="wrapper"></div>
+                <div className="wrapper">
+                    <header>
+                        <h2>Projects</h2>
+                    </header>
+                    <div className="content">
+                        <div className="cards">
+                            {demoProjects?.map((project, index) => {
+                                return <ProjectCard name={project.name} description={project.description} image={project.image} technologies={project.technologies} github={project.github} demo={project.demo} key={index} />
+                            })}
+                        </div>
+                    </div>
+                </div>
             </section>
         </>
     )
