e195b042b4a3bd90e73d7d4b2446a3095687df39
Charanteja <37008444+charan379@users.noreply.github.com>
2023-07-12 07:42:10 +0000

    reduce min offsetY to 350

--- src/lib/utils/handleBacktotopOnScroll.ts
+++ src/lib/utils/handleBacktotopOnScroll.ts
@@ -1,6 +1,6 @@
 export function handleBacktotopOnScroll(backToTopButtonId: string, scrollY: number) {
     const backToTopButton = document.querySelector(backToTopButtonId);
-    if (scrollY > 550) {
+    if (scrollY > 350) {
         backToTopButton?.classList.add("show");
     } else {
         backToTopButton?.classList.remove("show");
