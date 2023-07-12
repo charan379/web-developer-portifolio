4b1999ed391ddf00fd775b8acb2f56ed08668652
Charanteja <37008444+charan379@users.noreply.github.com>
2023-07-12 07:04:42 +0000

    added useScroll

--- src/components/Buttons/BackToTopBtn.tsx
+++ src/components/Buttons/BackToTopBtn.tsx
@@ -1,7 +1,23 @@
+'use client';
+
 import { UpArrowSvgIcon } from '@/assets/icons/UpArrow'
-import React from 'react'
+import { useScroll } from '@/lib/hooks/useScroll';
+import { handleBacktotopOnScroll } from '@/lib/utils/handleBacktotopOnScroll';
+import React, { useEffect } from 'react'
 
 const BackToTopBtn = () => {
+
+    const scroll = useScroll();
+
+    useEffect(() => {
+        handleBacktotopOnScroll("#backToTopButton", scroll.Y)
+        console.log(scroll.Y)
+        return () => {
+        }
+
+    }, [scroll.Y])
+
+
     return (
         <>
             <a id="backToTopButton" title="Back to the top" href="#home">
