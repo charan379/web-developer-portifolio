2df59fc79d879844871f36d14857017d950c2e1f
charan379 <37008444+charan379@users.noreply.github.com>
2023-08-05 23:21:19 +0530

    minor tweaks

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -5,56 +5,7 @@ import styles from "./Projects.module.css";
 
 const Projects = () => {
 
-    const demoProjects: Array<Project> = [
-        {
-            name: 'Orange Evolution',
-            description: 'Study platform developed in the Hackathon of the FCamara Training Program 4.',
-            image: './assets/images/projects/orange-evolution.gif',
-            technologies: ["HTML", "CSS", "JS", "NodeJS", "Express", "SQLite"],
-            github: 'https://github.com/squad-30/OrangeEvolution-Squad30',
-            demo: 'https://orangeevolution-squad30.up.railway.app'
-        },
-        {
-            name: '21 day code challenge',
-            description: 'Challenge promoted by Rocketseat with 21 different projects.',
-            image: './assets/images/projects/desafio-de-codigo.jpg',
-            technologies: ["HTML", "CSS", "JS", "NodeJS", "Express", "SQLite"],
-            github: 'https://github.com/squad-30/OrangeEvolution-Squad30',
-            demo: 'https://orangeevolution-squad30.up.railway.app'
-        },
-        {
-            name: 'Nutritionist Roberto - Links',
-            description: 'Link pages developed for client.',
-            image: './assets/images/projects/roberto-links.gif',
-            technologies: ["HTML", "CSS", "JS"],
-            github: 'https://github.com/squad-30/OrangeEvolution-Squad30',
-            demo: 'https://orangeevolution-squad30.up.railway.app'
-        },
-        {
-            name: 'Nutritionist Roberto - Consultation page',
-            description: 'Contact page for queries developed for customers.',
-            image: './assets/images/projects/roberto-consulta.gif',
-            technologies: ["HTML", "CSS", "JS", "NodeJS"],
-            github: 'https://github.com/squad-30/OrangeEvolution-Squad30',
-            demo: 'https://orangeevolution-squad30.up.railway.app'
-        },
-        {
-            name: 'Doctor Care',
-            description: 'Website project developed at Rocketseats Next Level Week Return',
-            image: './assets/images/projects/doctorcare.gif',
-            technologies: ["HTML", "CSS", "JS"],
-            github: 'https://github.com/squad-30/OrangeEvolution-Squad30',
-            demo: 'https://orangeevolution-squad30.up.railway.app'
-        },
-        {
-            name: 'Orange Evolution',
-            description: 'Study platform developed in the Hackathon of the FCamara Training Program 4.',
-            image: './assets/images/projects/orange-evolution.gif',
-            technologies: ["HTML", "CSS", "JS", "NodeJS", "Express", "SQLite"],
-            github: 'https://github.com/squad-30/OrangeEvolution-Squad30',
-            demo: 'https://orangeevolution-squad30.up.railway.app'
-        }
-    ]
+    const demoProjects: Array<Project> = [];
     return (
         <>
             <section id="projects" className={styles.projects}>
--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -5,23 +5,7 @@ import styles from "./Skills.module.css"
 
 const Skills = () => {
 
-    const demoSkills: Skill[] = [
-        {
-            name: "HTML5",
-            description: "HTML5 is a hypertext markup language, which we use totag the elements of the page and build it.",
-            image: "./assets/images/html.png",
-        },
-        {
-            name: "CSS3",
-            description: "CSS3 is a style language used to describe the presentation of page elements by styling them.",
-            image: "./assets/images/css.png",
-        },
-        {
-            name: "JavaScript",
-            description: "JavaScript is an interpreted programming language used in web development to describe features and page behaviors.",
-            image: "./assets/images/js.png",
-        },
-    ]
+    const demoSkills: Skill[] = [];
     return (
         <>
             <section id="skills" className={styles.skills}>
