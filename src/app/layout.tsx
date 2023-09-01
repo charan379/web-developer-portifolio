c28f5d960c1e1abcc2a44a2be8c2648ff27728f3
Charanteja <37008444+charan379@users.noreply.github.com>
2023-09-01 13:06:38 +0000

    changing default theme to light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`} data-theme="dark">
+      <body className={`${inter.className}`} data-theme="light">
         {children}
       </body>
     </html>
