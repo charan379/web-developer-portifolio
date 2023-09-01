8726eea25abd1947248cf128c30162780e582e23
Charanteja <37008444+charan379@users.noreply.github.com>
2023-09-01 07:31:56 +0000

    profile card added

---
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from "./ProfileCard.module.css"
+
+const ProfileCard = () => {
+    return (
+        <div className={styles.profileCardPage}>
+            <div className={styles.profileCardModal}>
+                sdfdsf
+            </div>
+        </div>
+    )
+}
+
+export default ProfileCard;
