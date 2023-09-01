e46ef289698e32b77edbb19959b10d43bf9480b3
Charanteja <37008444+charan379@users.noreply.github.com>
2023-09-01 07:31:44 +0000

    comenting out code to make space for profile card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,89 +9,91 @@ import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import ThemeToggle from "@/components/ThemeToggle";
 import BackToTopBtn from "@/components/Buttons/BackToTopBtn";
+import ProfileCard from "@/components/ProfileCard/ProfileCard";
 
-export async function generateMetadata() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/data`, {
-    method: "GET",
-  });
+// export async function generateMetadata() {
+//   const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/data`, {
+//     method: "GET",
+//   });
 
-  const data = await response.json();
+//   const data = await response.json();
 
-  return {
-    // meta tags
-    title: data?.profile?.name,
-    description: data?.profile?.aboutMe,
-    // pwa
-    applicationName: data?.profile?.name,
-    appleWebApp: {
-      capable: true,
-      title: data?.profile?.name,
-      statusBarStyle: "default",
-    },
-    formatDetection: {
-      telephone: false,
-    },
-    themeColor: "#e9f1fa",
-    viewport:
-      "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
-    manifest: "/manifest.json",
-    icons: [
-      {
-        rel: "apple-touch-icon",
-        url: "/assets/images/site/apple-touch-icon.png",
-      },
-      { rel: "shortcut icon", url: "/favicon.ico" },
-    ],
-    keywords: ["web-developer", "portifolio", "projects", "skills"],
+//   return {
+//     // meta tags
+//     title: data?.profile?.name,
+//     description: data?.profile?.aboutMe,
+//     // pwa
+//     applicationName: data?.profile?.name,
+//     appleWebApp: {
+//       capable: true,
+//       title: data?.profile?.name,
+//       statusBarStyle: "default",
+//     },
+//     formatDetection: {
+//       telephone: false,
+//     },
+//     themeColor: "#e9f1fa",
+//     viewport:
+//       "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+//     manifest: "/manifest.json",
+//     icons: [
+//       {
+//         rel: "apple-touch-icon",
+//         url: "/assets/images/site/apple-touch-icon.png",
+//       },
+//       { rel: "shortcut icon", url: "/favicon.ico" },
+//     ],
+//     keywords: ["web-developer", "portifolio", "projects", "skills"],
 
-    // og meta tags
-    openGraph: {
-      title: data?.profile?.name,
-      description: data?.profile?.aboutMe,
-      url: process.env.NEXT_PUBLIC_BASE_URL,
-      siteName: data?.profile?.name,
-      images: [
-        {
-          url: `${process.env.NEXT_PUBLIC_BASE_URL}${data?.profile?.displayPicture}`,
-          width: 185,
-          height: 278,
-        },
-        {
-          url: `${process.env.NEXT_PUBLIC_BASE_URL}${data?.profile?.displayPicture}`,
-          width: 342,
-          height: 513,
-        },
-        {
-          url: `${process.env.NEXT_PUBLIC_BASE_URL}${data?.profile?.displayPicture}`,
-          width: 500,
-          height: 750,
-        },
-      ],
-      locale: "en-US",
-      type: "website",
-    },
-    // twitter og meta tags
-    twitter: {
-      card: "summary_large_image",
-      title: data?.profile?.name,
-      description: data?.profile?.aboutMe,
-      images: [
-        `${process.env.NEXT_PUBLIC_BASE_URL}${data?.profile?.displayPicture}`,
-      ],
-    },
-  };
-}
+//     // og meta tags
+//     openGraph: {
+//       title: data?.profile?.name,
+//       description: data?.profile?.aboutMe,
+//       url: process.env.NEXT_PUBLIC_BASE_URL,
+//       siteName: data?.profile?.name,
+//       images: [
+//         {
+//           url: `${process.env.NEXT_PUBLIC_BASE_URL}${data?.profile?.displayPicture}`,
+//           width: 185,
+//           height: 278,
+//         },
+//         {
+//           url: `${process.env.NEXT_PUBLIC_BASE_URL}${data?.profile?.displayPicture}`,
+//           width: 342,
+//           height: 513,
+//         },
+//         {
+//           url: `${process.env.NEXT_PUBLIC_BASE_URL}${data?.profile?.displayPicture}`,
+//           width: 500,
+//           height: 750,
+//         },
+//       ],
+//       locale: "en-US",
+//       type: "website",
+//     },
+//     // twitter og meta tags
+//     twitter: {
+//       card: "summary_large_image",
+//       title: data?.profile?.name,
+//       description: data?.profile?.aboutMe,
+//       images: [
+//         `${process.env.NEXT_PUBLIC_BASE_URL}${data?.profile?.displayPicture}`,
+//       ],
+//     },
+//   };
+// }
 
 export default async function Home() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/data`, {
-    method: "GET",
-  });
+  // const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/data`, {
+  //   method: "GET",
+  // });
 
-  const data = await response.json();
+  // const data = await response.json();
 
   return (
     <>
-      <NavBar profile={data?.profile} />
+      <ProfileCard />
+      {/* <NavBar profile={data?.profile} />
 
       <Intro profile={data?.profile} />
 
@@ -105,7 +107,7 @@ export default async function Home() {
 
       <BackToTopBtn />
 
-      <Footer profile={data?.profile} />
+      <Footer profile={data?.profile} /> */}
 
       <ThemeToggle />
     </>
