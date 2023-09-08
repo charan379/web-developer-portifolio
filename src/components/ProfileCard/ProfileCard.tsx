116995d1e97b3b07d25b1a7b5dfd18720042a0b5
Charanteja <37008444+charan379@users.noreply.github.com>
2023-09-08 13:19:09 +0000

    profile grid and moveIn profile areas animation

--- src/components/ProfileCard/ProfileCard.tsx
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -5,7 +5,13 @@ const ProfileCard = () => {
     return (
         <div className={styles.profileCardPage}>
             <div className={styles.profileCardModal}>
-
+                <div className={styles.profile}>
+                    <div className={styles.gridItem1}> Grid 1 </div>
+                    <div className={styles.gridItem2}> Grid 2 </div>
+                    <div className={styles.gridItem3}> Grid 3 </div>
+                    <div className={styles.gridItem4}> Grid 4 </div>
+                    <div className={styles.gridItem5}> Grid 5 </div>
+                </div>
             </div>
         </div>
     )
