298f15e598dca6bfe950b78595f70439024ae2ed
Charanteja <37008444+charan379@users.noreply.github.com>
2023-09-11 11:03:18 +0000

    added social links

--- src/components/ProfileCard/ProfileCard.tsx
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./ProfileCard.module.css";
 import ProfilePhoto from "../ProfilePhoto/ProfilePhoto";
+import SocialLinks from "../SocialLinks";
 
 const ProfileCard = () => {
   return (
@@ -13,7 +14,9 @@ const ProfileCard = () => {
           <div className={styles.gridItem2}> Grid 2 </div>
           <div className={styles.gridItem3}> Grid 3 </div>
           <div className={styles.gridItem4}> Grid 4 </div>
-          <div className={styles.gridItem5}> Grid 5 </div>
+          <div className={styles.gridItem5}>
+            <SocialLinks />
+          </div>
         </div>
       </div>
     </div>
