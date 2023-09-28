eafe30e4263ecf9542f52ca88cd8c5f205e6a04d
charan379 <37008444+charan379@users.noreply.github.com>
2023-09-28 12:22:32 +0530

    btn-anime-i

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gradient-to-br from-pink-400 via-purple-400 to-indigo-400`}>
+      <body
+        className={`${inter.className} bg-gradient-to-br from-pink-400 via-purple-400 to-indigo-400`}
+      >
         {children}
       </body>
     </html>
--- src/components/ProfileCard/ProfileCard.tsx
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -6,9 +6,9 @@ import Bio from "../Bio/Bio";
 
 const ProfileCard = () => {
   return (
-    <div className="max-w-2xl m-5 bg-slate-50 rounded-3xl shadow-2xl p-5 2xl:max-w-screen-lg">
+    <div className="max-w-2xl 2xl:max-w-screen-lg m-5 bg-slate-50 rounded-3xl border-red-600 shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] p-5">
       <div>
-        <div >
+        <div>
           {/* Profile Photo Component */}
           <ProfilePhoto style={{ width: "140px", height: "140px" }} />
         </div>
@@ -17,7 +17,14 @@ const ProfileCard = () => {
           <Bio />
         </div>
       </div>
-      <div > Grid 2 </div>
+      <div className="my-3">
+        <a
+          href="#"
+          className="bg-gradient-to-r from-red-500 via-pink-800 to-slate-800 p-2 my-1 rounded-md text-slate-50 btn-anime-1 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]"
+        >
+          Button Link
+        </a>
+      </div>
       <div> Grid 3 </div>
       <div> Grid 4 </div>
       <div>
