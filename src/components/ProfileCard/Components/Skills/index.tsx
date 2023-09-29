951e9009c98c72f2b319e9b6f9bc6a909d14f62a
charan379 <37008444+charan379@users.noreply.github.com>
2023-09-29 13:45:47 +0530

    new component Skills

--- src/components/ProfileCard/Components/Skills/index.tsx
+++ src/components/ProfileCard/Components/Skills/index.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import Skill from "./Skill";
 
-const Skills = () => {
-  return (
-    <div>Skills</div>
-  )
+interface SkillsCompProps extends ComponentProps {
+  skills: string[];
+  skillClassName?: string;
 }
 
-export default Skills
+const Skills: React.FC<SkillsCompProps> = (props) => {
+  return (
+    <div className={props?.className ?? ""} style={props?.style}>
+      {props.skills?.map((skill, index) => {
+        return (
+          <Skill skill={skill} key={index} className={props?.skillClassName} />
+        );
+      })}
+    </div>
+  );
+};
+
+export default Skills;
