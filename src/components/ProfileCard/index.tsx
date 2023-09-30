101c1f19d571a22a8300d1014b866a1269402137
charan379 <37008444+charan379@users.noreply.github.com>
2023-09-30 15:13:39 +0530

    added profile-card-init-anime-1

--- src/components/ProfileCard/index.tsx
+++ src/components/ProfileCard/index.tsx
@@ -8,7 +8,7 @@ import RightSection from "./Components/RLSections/RightSection";
 const ProfileCard = () => {
   return (
     // profile card
-    <div className="w-11/12 pt-2 md:pt-3 flex md:gap-y-3 flex-col items-center justify-center max-w-2xl 2xl:max-w-screen-lg m-5 bg-slate-50 rounded-3xl border-red-600 shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]">
+    <div className="w-11/12 pt-2 md:pt-3 flex md:gap-y-3 flex-col items-center justify-center max-w-2xl 2xl:max-w-screen-lg m-5 bg-slate-50 rounded-xl shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] profile-card-init-anime-1">
       {/* Bio */}
       <Bio className="p-2 flex flex-col md:flex-row gap-3 md:gap-0 items-center" />
       {/* AboutMe */}
@@ -22,7 +22,7 @@ const ProfileCard = () => {
       </div>
 
       {/* social links */}
-      <div className="p-2 w-full bg-slate-200 rounded-b-3xl flex flex-row flex-wrap items-center justify-center border-t-2">
+      <div className="p-2 w-full bg-slate-200 rounded-b-xl flex flex-row flex-wrap items-center justify-center border-t-2">
         <SocialLinks />
       </div>
     </div>
