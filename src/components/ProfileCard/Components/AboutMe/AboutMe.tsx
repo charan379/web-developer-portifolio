862a24db8aa7b5775d8498da58527efe90ed7e66
charan379 <37008444+charan379@users.noreply.github.com>
2023-09-30 21:11:30 +0530

    added div style and content class

--- src/components/ProfileCard/Components/AboutMe/AboutMe.tsx
+++ src/components/ProfileCard/Components/AboutMe/AboutMe.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 
-const AboutMe: React.FC<ComponentProps> = (props) => {
+interface AboutMeProps extends ComponentProps {
+  contentClassName?: string;
+  contentStyle?: React.CSSProperties;
+}
+const AboutMe: React.FC<AboutMeProps> = (props) => {
   return (
     <>
-      <h4 className={`${props.className}`}>
-        I am an enthusiastic Learner/Developer dedicated to a journey of
-        continuous growth and progress within the ever-evolving landscape of
-        technology and development. My passion lies in not only creating
-        innovative solutions but also in the process of acquiring new knowledge
-        and skills.
-      </h4>
+      <div className={props?.className}>
+        <h4 className={`${props?.contentClassName}`}>
+          I am an enthusiastic Learner/Developer dedicated to a journey of
+          continuous growth and progress within the ever-evolving landscape of
+          technology and development. My passion lies in not only creating
+          innovative solutions but also in the process of acquiring new
+          knowledge and skills.
+        </h4>
+      </div>
     </>
   );
 };
