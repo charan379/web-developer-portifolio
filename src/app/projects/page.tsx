a7c9cf21b74e27cdf3259124ffc4b521c823b000
charan379 <37008444+charan379@users.noreply.github.com>
2023-10-01 11:10:54 +0530

    added pojects page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,7 @@
+export default async function ProjectsPage() {
+  return (
+    <main className="min-h-[100dvh] w-full m-0 flex flex-row justify-center items-center overflow-auto">
+      <h1>Projects Page</h1>
+    </main>
+  );
+}
