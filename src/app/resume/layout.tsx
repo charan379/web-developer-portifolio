278e5d5edc0337535cd2faa59daa3d405837fe9a
Charanteja <37008444+charan379@users.noreply.github.com>
2023-10-04 10:08:27 +0000

    bug fix

--- src/app/resume/layout.tsx
+++ src/app/resume/layout.tsx
@@ -1,17 +1,3 @@
-import "../globals.css";
-import { Open_Sans } from "next/font/google";
-
-const googleFont = Open_Sans({ weight: "400", subsets: ["latin"] });
-
-
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body
-        className={`${googleFont.className} min-h-[100%] bg-slate-200 overflow-y-auto`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+  return <>{children}</>;
 }
