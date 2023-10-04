11bb32b54103da1e6eb9ba48133bb259b55a15e1
Charanteja <37008444+charan379@users.noreply.github.com>
2023-10-04 09:57:21 +0000

    layout for resume

--- src/app/resume/layout.tsx
+++ src/app/resume/layout.tsx
@@ -1,3 +1,17 @@
+import "../globals.css";
+import { Open_Sans } from "next/font/google";
+
+const googleFont = Open_Sans({ weight: "400", subsets: ["latin"] });
+
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return (
+    <html lang="en">
+      <body
+        className={`${googleFont.className} min-h-[100%] bg-slate-200 overflow-y-auto`}
+      >
+        {children}
+      </body>
+    </html>
+  );
 }
