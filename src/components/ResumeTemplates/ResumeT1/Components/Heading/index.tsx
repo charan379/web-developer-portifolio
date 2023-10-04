5522a049b8916bc3270d2d26ad2e15ff291766c6
Charanteja <37008444+charan379@users.noreply.github.com>
2023-10-04 12:07:18 +0000

    new component

---
+++ src/components/ResumeTemplates/ResumeT1/Components/Heading/index.tsx
@@ -0,0 +1,15 @@
+import React, { PropsWithChildren } from 'react'
+
+
+interface HeadingProps extends ComponentProps, PropsWithChildren { };
+
+const Heading: React.FC<HeadingProps> = (props) => {
+    const { children, className, style } = props;
+    return (
+        <h2 className={className ?? "text-gray-900 font-bold text-xl rt1-heading uppercase mb-2 tracking-widest break-words"} style={style}>
+            {children}
+        </h2>
+    )
+}
+
+export default Heading;
