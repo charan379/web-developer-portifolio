ab1cfc5e78d81c9dfa1e9c18c55e0dc88012b58a
Charanteja <37008444+charan379@users.noreply.github.com>
2023-10-04 12:58:31 +0000

    style updated

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -2,7 +2,7 @@ import ResumeT1 from "@/components/ResumeTemplates/ResumeT1";
 
 export default async function ResumePage() {
   return (
-    <main className="mx-auto bg-slate-100 overflow-auto">
+    <main className="min-h-[100dvh] w-full mx-auto px-2 bg-slate-100 flex flex-row justify-center items-center overflow-auto">
       <ResumeT1 />
     </main>
   );
