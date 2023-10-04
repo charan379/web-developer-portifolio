f4fbdfb4f952d5a0e25bdcf882e674cd9e8d10cf
Charanteja <37008444+charan379@users.noreply.github.com>
2023-10-04 12:53:23 +0000

    bg-white , border radius

--- src/components/ResumeTemplates/ResumeT1/index.tsx
+++ src/components/ResumeTemplates/ResumeT1/index.tsx
@@ -10,35 +10,34 @@ import Heading from "./Components/Heading";
 
 const ResumeT1: React.FC = () => {
   return (
-    <>
-      <div className="text-slate-400 mx-auto my-5 max-w-5xl aspect-A4-Portrait p-4 sm:p-8 container bg-slate-50 flex flex-col border-4 border-violet-600">
-
-        {/* Header 
+    <div className="text-slate-400 mx-auto my-5 max-w-5xl aspect-A4-Portrait p-4 sm:p-8 container bg-white rounded-lg shadow-xl flex flex-col ">
+      
+      {/* Header 
             basic classes to be present for styling
             [w-full, p-1, max-h-[20%]]
-        
         */}
-        <Header className="w-full p-1 max-h-[20%] flex flex-col items-start justify-between gap-0 sm:gap-1 mb-1 align-top" />
+      <Header className="w-full p-1 max-h-[20%] flex flex-col items-start justify-between gap-0 sm:gap-1 mb-1 align-top" />
 
-        {/* content
+      {/* content
             basic classes to be present for styling
             [w-full, min-h-full, max-w-full, flex, flex-row]
         */}
-        <Contect className="w-full min-h-full max-w-full flex flex-row border-t-2 border-gray-200">
-          {/* left section */}
-          <div className="min-w-[35%] max-h-[35%] min-h-full pt-2 sm:pt-4 border-r-0 border-gray-200">
-            <div>
-              <Heading>Details</Heading>
-              <ContactInformation />
-            </div>
+      <Contect className="w-full min-h-full max-w-full flex flex-row border-t-2 border-gray-200">
+       
+        {/* left section */}
+        <div className="min-w-[35%] max-h-[35%] min-h-full pt-2 sm:pt-4 border-r-0 border-gray-200">
+          <div>
+            <Heading>Details</Heading>
+            <ContactInformation />
           </div>
-          {/* right section */}
-          <div className="min-w-[65%] min-h-full pt-2 sm:pt-4 border-l-2 border-gray-200">
+        </div>
+        
+        {/* right section */}
+        <div className="min-w-[65%] min-h-full pt-2 sm:pt-4 border-l-2 border-gray-200">
 
-          </div>
-        </Contect>
-      </div>
-    </>
+        </div>
+      </Contect>
+    </div>
   )
 };
 
