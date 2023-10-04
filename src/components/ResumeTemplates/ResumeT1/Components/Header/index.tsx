172b49b53d5443c5b05d2deb5b5e4a77be80aef8
Charanteja <37008444+charan379@users.noreply.github.com>
2023-10-04 07:05:04 +0000

    RT1 header updated

--- src/components/ResumeTemplates/ResumeT1/Components/Header/index.tsx
+++ src/components/ResumeTemplates/ResumeT1/Components/Header/index.tsx
@@ -2,24 +2,21 @@ import React from "react";
 
 const Header: React.FC<ComponentProps> = (props) => {
   return (
-    <header className={props?.className ?? "inline-flex items-center justify-between w-full mb-1 align-top border-b-4 border-gray-300"}
+    <header className={props?.className ?? "inline-flex flex-col items-start justify-between w-full mb-1 align-top border-b-4 border-gray-300"}
       style={props?.style}
     >
-      <section className="flex flex-col gap-1">
-        {/* name */}
-        <h1 className="font-bold text-3xl sm:text-5xl text-gray-700">
-          Charanteja Yandrapti
-        </h1>
-        {/* Job Title / profession */}
-        <h2 className="ml-1 font-semibold text-1xl sm:text-2xl text-gray-700">
-          Full Stack Web Development
-        </h2>
-        {/* location */}
-        <h3 className="ml-1 font-semibold text-sm sm:text-lg text-gray-500">
-          Chennai, India &#127760;
-        </h3>
-      </section>
-      {/* right block */}
+      {/* name */}
+      <h1 className="font-bold text-2xl sm:text-5xl text-gray-700">
+        Charanteja Yandrapti
+      </h1>
+      {/* Job Title / profession */}
+      <h2 className="sm:ml-1 font-semibold text-[14px] sm:text-xl text-gray-700">
+        Full Stack Web Development
+      </h2>
+      {/* location */}
+      <h3 className="sm:ml-1 font-semibold text-sm sm:text-base text-gray-500">
+        Chennai, India &#127760;
+      </h3>
     </header>
   );
 };
