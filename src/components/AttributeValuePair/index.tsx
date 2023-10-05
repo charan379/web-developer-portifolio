bc773e92747cfcb4ca2c166f4816c9d1a2537084
charan379 <37008444+charan379@users.noreply.github.com>
2023-10-05 12:06:55 +0530

    support reactNode as value

--- src/components/AttributeValuePair/index.tsx
+++ src/components/AttributeValuePair/index.tsx
@@ -1,40 +1,79 @@
 import React from "react";
 
 export interface AttributeValuePairCompProps extends ComponentProps {
-    attributeClassName?: string;
-    attributeStyle?: React.CSSProperties;
-    valueClassName?: string;
-    valueStyle?: React.CSSProperties;
-    AttributeAndValue: {
-        attribute: string, value: React.ReactElement | string
-    };
-    separator?: React.ReactElement | string,
-    separatorClassName?: string,
-    separatorStyle?: React.CSSProperties,
-    key: string | number;
+  attributeClassName?: string;
+  attributeStyle?: React.CSSProperties;
+  valueClassName?: string;
+  valueStyle?: React.CSSProperties;
+  AttributeAndValue: {
+    attribute: string;
+    value: React.ReactElement | string | React.ReactNode;
+  };
+  separator?: React.ReactElement | string | React.ReactNode;
+  separatorClassName?: string;
+  separatorStyle?: React.CSSProperties;
+  key: string | number;
 }
 
 const AttributeValuePair: React.FC<AttributeValuePairCompProps> = (props) => {
+  const {
+    key,
+    className,
+    style,
+    attributeClassName,
+    attributeStyle,
+    valueClassName,
+    valueStyle,
+    AttributeAndValue,
+    separator,
+    separatorClassName,
+    separatorStyle,
+  } = props;
 
-    const { key = "kvp1", className, style, attributeClassName, attributeStyle, valueClassName, valueStyle, AttributeAndValue, separator, separatorClassName, separatorStyle } = props;
+  return (
+    <div
+      className={
+        className ??
+        "max-w-full md:max-w-[50%] min-w-[50%] text-sm md:text-base p-1 flex flex-row flex-nowrap overflow-hidden break-words"
+      }
+      key={key}
+      style={style}
+    >
+      {/* key */}
+      <span
+        id="key"
+        className={
+          attributeClassName ?? "min-w-[65px]  text-left font-bold break-words "
+        }
+        style={attributeStyle}
+      >
+        {AttributeAndValue.attribute}
+      </span>
+      {/* separator */}
+      {typeof separator !== "string" ? (
+        separator
+      ) : (
+        <span
+          id="separator"
+          className={separatorClassName ?? "mx-1 text-left font-bold"}
+          style={separatorStyle}
+        >
+          {separator ?? ":"}
+        </span>
+      )}
 
-    return (
-        <div className={className ?? "max-w-full md:max-w-[50%] min-w-[50%] text-sm md:text-base p-1 flex flex-row flex-nowrap overflow-hidden break-words"} key={key} style={style}>
-            {/* key */}
-            <span id="key" className={attributeClassName ?? "min-w-[65px]  text-left font-bold break-words "} style={attributeStyle}>
-                {AttributeAndValue.attribute}
-            </span>
-            {/* separator */}
-            {typeof separator !== "string" ? separator : <span id="separator" className={separatorClassName ?? "mx-1 text-left font-bold"} style={separatorStyle}>
-                {separator ?? ":"}
-            </span>}
-
-            {/* value */}
-            <span id="value" className={valueClassName ?? "max-w-full md:max-w-[50%] text-left break-words"} style={valueStyle}>
-                {AttributeAndValue.value}
-            </span>
-        </div>
-    )
-}
+      {/* value */}
+      <span
+        id="value"
+        className={
+          valueClassName ?? "max-w-full md:max-w-[50%] text-left break-words"
+        }
+        style={valueStyle}
+      >
+        {AttributeAndValue.value}
+      </span>
+    </div>
+  );
+};
 
 export default AttributeValuePair;
