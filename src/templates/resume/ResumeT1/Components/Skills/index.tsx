8198e31371099964a97065cbb32eaa6c0b2ecd86
charan379 <37008444+charan379@users.noreply.github.com>
2023-10-05 13:08:19 +0530

    skills updated

--- src/templates/resume/ResumeT1/Components/Skills/index.tsx
+++ src/templates/resume/ResumeT1/Components/Skills/index.tsx
@@ -1,79 +1,44 @@
 import React from "react";
+import Heading from "../Heading";
 
 const Skills = () => {
+  const skills: string[] = [
+    "HTML5",
+    "CSS3",
+    "Prompt Engineering",
+    "Containerization",
+    "CI/CD",
+    "System Administration",
+    "BASH",
+    "AWS",
+    "Linux",
+    "Javascript",
+    "Typescript",
+    "Node.js",
+    "React.js",
+    "Java",
+    "NoSQL",
+    "MongoDB",
+  ];
+
   return (
-    <section className="pb-6 mb-4 mt-0 first:mt-0 break-inside-avoid border-b-4 border-gray-300 ">
+    <section className="pb-6 mb-4 mt-0 first:mt-0 ">
       {/* To keep in the same column */}
-      <section className="break-inside-avoid">
-        <h2 className="mb-2 text-lg font-bold tracking-widest text-gray-700 print:font-normal">
-          SKILLS
-        </h2>
-        <section className="mb-0 break-inside-avoid">
+      <section className="">
+        <Heading>Skills</Heading>
+        <section className="mb-0">
           <section className="mt-1 last:pb-1">
-            <ul className="flex flex-wrap text-md leading-relaxed -mr-1.6 -mb-1 font-bold">
-              <li className="p-1.5 mr-1.6 mb-1 text-white leading-relaxed print:bg-white print:border-inset bg-gray-800">
-                HTML5
-              </li>
-              <li className="p-1.5 mr-1.6 mb-1 text-white leading-relaxed print:bg-white print:border-inset bg-gray-800">
-                CSS3
-              </li>
-              <li className="p-1.5 mr-1.6 mb-1 text-white leading-relaxed print:bg-white print:border-inset bg-gray-800">
-                SCSS
-              </li>
-              <li className="p-1.5 mr-1.6 mb-1 text-white leading-relaxed print:bg-white print:border-inset bg-gray-800">
-                Prompt Engineering
-              </li>
-              <li className="p-1.5 mr-1.6 mb-1 text-white leading-relaxed print:bg-white print:border-inset bg-gray-800">
-                Containerization
-              </li>
-
-              <li className="p-1.5 mr-1.6 mb-1 text-white leading-relaxed print:bg-white print:border-inset bg-gray-800">
-                CI/CD
-              </li>
-              <li className="p-1.5 mr-1.6 mb-1 text-white leading-relaxed print:bg-white print:border-inset bg-gray-800">
-                System Administration
-              </li>
-              <li className="p-1.5 mr-1.6 mb-1 text-white leading-relaxed print:bg-white print:border-inset bg-gray-800">
-                BASH
-              </li>
-
-              <li className="p-1.5 mr-1.6 mb-1 text-white leading-relaxed print:bg-white print:border-inset bg-gray-800">
-                AWS
-              </li>
-              <li className="p-1.5 mr-1.6 mb-1 text-white leading-relaxed print:bg-white print:border-inset bg-gray-800">
-                Linux
-              </li>
-              <li className="p-1.5 mr-1.6 mb-1 text-white leading-relaxed print:bg-white print:border-inset bg-gray-800">
-                Javascript
-              </li>
-
-              <li className="p-1.5 mr-1.6 mb-1 text-white leading-relaxed print:bg-white print:border-inset bg-gray-800">
-                Typescript
-              </li>
-              <li className="p-1.5 mr-1.6 mb-1 text-white leading-relaxed print:bg-white print:border-inset bg-gray-800">
-                Node.js
-              </li>
-              <li className="p-1.5 mr-1.6 mb-1 text-white leading-relaxed print:bg-white print:border-inset bg-gray-800">
-                React.js
-              </li>
-              <li className="p-1.5 mr-1.6 mb-1 text-white leading-relaxed print:bg-white print:border-inset bg-gray-800">
-                Python
-              </li>
-              <li className="p-1.5 mr-1.6 mb-1 text-white leading-relaxed print:bg-white print:border-inset bg-gray-800">
-                NoSQL
-              </li>
-              <li className="p-1.5 mr-1.6 mb-1 text-white leading-relaxed print:bg-white print:border-inset bg-gray-800">
-                Postgresql
-              </li>
-              <li className="p-1.5 mr-1.6 mb-1 text-white leading-relaxed print:bg-white print:border-inset bg-gray-800">
-                MongoDB
-              </li>
-              <li className="p-1.5 mr-1.6 mb-1 text-white leading-relaxed print:bg-white print:border-inset bg-gray-800">
-                Lua
-              </li>
-              <li className="p-1.5 mr-1.6 mb-1 text-white leading-relaxed print:bg-white print:border-inset bg-gray-800">
-                Functional Programming
-              </li>
+            <ul className="flex flex-wrap gap-1 text-md -mb-1 font-bold">
+              {skills.map((skill, index) => {
+                return (
+                  <li
+                    key={index}
+                    className="p-1.5 mb-1 rounded-sm text-white print:text-gray-900 bg-gray-800 print:bg-white print:border-gray-200 print:border-2 cursor-pointer"
+                  >
+                    {skill}
+                  </li>
+                );
+              })}
             </ul>
           </section>
         </section>
