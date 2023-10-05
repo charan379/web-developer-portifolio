64d385bf14436565f43ac7b4ec5ab420f0c2d606
charan379 <37008444+charan379@users.noreply.github.com>
2023-10-05 13:08:31 +0530

    heading updated

--- src/templates/resume/ResumeT1/Components/Heading/index.tsx
+++ src/templates/resume/ResumeT1/Components/Heading/index.tsx
@@ -8,7 +8,7 @@ const Heading: React.FC<HeadingProps> = (props) => {
     <h2
       className={
         className ??
-        "text-gray-900 font-bold font-montserrat text-xs sm:text-xl uppercase mb-4 sm:mb-6 tracking-wide break-words cursor-pointer rt1-heading after:border-b-[0.1rem] sm:after:border-b-[0.15rem]"
+        "text-gray-900 font-bold font-montserrat text-xs sm:text-lg uppercase mb-4 sm:mb-6 tracking-widest break-words cursor-pointer rt1-heading after:border-b-[0.1rem] sm:after:border-b-[0.15rem]"
       }
       style={style}
     >
