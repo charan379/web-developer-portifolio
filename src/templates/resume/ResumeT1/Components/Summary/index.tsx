20345c56915a88b0f5743e24d454ca4898823a3b
Charanteja <37008444+charan379@users.noreply.github.com>
2023-10-06 10:00:00 +0000

    summary updated

--- src/templates/resume/ResumeT1/Components/Summary/index.tsx
+++ src/templates/resume/ResumeT1/Components/Summary/index.tsx
@@ -1,26 +1,19 @@
 import React from "react";
+import Heading from "../Heading";
 
 
 const Summary: React.FC<ComponentProps> = (props) => {
 
   const { className, style } = props;
   return (
-    <div className={className ?? "pb-2 border-b-4 border-gray-300"} style={style}>
-      {/* To keep in the same column */}
-      <section className="break-inside-avoid">
-        <h2 className="mb-2 text-xl font-bold tracking-widest text-gray-700 print:font-normal">
-          SUMMARY
-        </h2>
-
-        <section className="mb-2 break-inside-avoid">
-          <p className="mt-2 px-4 leading-normal text-gray-700 text-base">
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem
-          </p>
-        </section>
-      </section>
+    <div className={className ?? "pb-2"} style={style}>
+      <Heading>Summary</Heading>
+      <p className="mt-2 px-2 leading-normal text-gray-400 text-base font-roboto400">
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+        commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+        penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+        Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem
+      </p>
     </div>
   );
 };
