cf6b67c8baba4af70679865a81af16b7e47a86e7
charan379 <37008444+charan379@users.noreply.github.com>
2023-10-12 09:44:24 +0530

    remove resume template

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,9 +1,12 @@
-import ResumeT1 from "@/templates/resume/ResumeT1";
+import Text from "@/components/Text";
+
 
 export default async function ResumePage() {
   return (
     <main className="min-h-[100dvh] w-full mx-auto px-2 bg-slate-100 flex flex-row justify-center items-center overflow-auto">
-      <ResumeT1 />
+      <Text>
+        Resume Will be displayed here
+      </Text>
     </main>
   );
 }
