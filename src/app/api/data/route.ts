f9f457e74218b0caa519ea2a53213fe98d371c96
charan379 <37008444+charan379@users.noreply.github.com>
2023-10-16 13:22:20 +0530

    sample db and 2s default timeout

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -1,8 +1,19 @@
 import { NextResponse } from "next/server";
 import data from "../../../../db.json";
+// import data from "./sampleDb.json";
+
 
 export async function GET(request: Request) {
+    const data: Object = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve((res: any) => {
+                res
+            })
+        }, 2000);
+    })
+
+    const res = await data;
     return NextResponse.json({
-        ...data
+        ...res
     })
 }
