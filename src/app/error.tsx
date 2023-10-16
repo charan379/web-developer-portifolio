d3d1660c13e42722ac9351e9a6fb0a417964a410
charan379 <37008444+charan379@users.noreply.github.com>
2023-10-16 13:21:21 +0530

    class updated

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -14,10 +14,12 @@ export default function Error({
   }, [error]);
 
   return (
-    <div className="mx-auto container text-center">
-      <h2>Something went wrong!</h2>
-      <h3>{error?.message}</h3>
-      <button onClick={() => reset()}>Try again</button>
+    <div className="min-h-[100dvh] w-full m-0 flex flex-row justify-center items-center overflow-auto">
+      <div className="p-2 text-center">
+        <h2>Something went wrong!</h2>
+        <h3>{error?.message}</h3>
+        <button onClick={() => reset()}>Try again</button>
+      </div>
     </div>
   );
 }
