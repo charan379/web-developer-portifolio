c2538e37c2cfd346a74d00e0238276192029531c
charan379 <37008444+charan379@users.noreply.github.com>
2023-10-16 13:24:23 +0530

    width and hieght updated

--- src/profilecard/containers/profilecard-container/index.tsx
+++ src/profilecard/containers/profilecard-container/index.tsx
@@ -11,8 +11,8 @@ const ProfilecardContainer: React.FC<PropsWithChildren> = ({ children }) => {
     // Render Profile card container with children
     <div
       className={twc(
-        "w-11/12 pt-6 flex  gap-y-2 flex-col items-center justify-center",
-        "max-h-[520px] max-w-[460px] m-5 bg-slate-50 rounded-xl",
+        "w-[95%] pt-6 flex gap-y-2 flex-col items-center justify-center",
+        "max-h-[720px] max-w-[480px] m-5 bg-slate-50 rounded-xl",
         "shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]",
         styles.profileCardInitAnime
       )}
