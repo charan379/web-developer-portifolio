e4b096dc74e3794b4fc1a1ada919a32926c6839b
charan379 <37008444+charan379@users.noreply.github.com>
2023-10-17 22:31:56 +0530

    mogodb impl

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -1,9 +1,12 @@
 import { NextResponse } from "next/server";
 // import data from "../../../../db.json";
 import data from "@/app/sampleDb.json";
+import EmployeeModel from "@/server/models/employeeModel";
+import connectToDatabase from "@/server/lib/utils/database";
 
 
 export async function GET(request: Request) {
+    await connectToDatabase();
     // const data: Object = new Promise((resolve, reject) => {
     //     setTimeout(() => {
     //         resolve((res: any) => {
@@ -11,6 +14,21 @@ export async function GET(request: Request) {
     //         })
     //     }, 100);
     // })
+
+    const details = [
+        { empId: 1234, name: 'Jack', designation: 'SE', salary: 23000 },
+        { empId: 4567, name: 'Johnson', designation: 'SSE', salary: 15000 },
+        { empId: 8910, name: 'Sachin', designation: 'TA', salary: 30000 }
+    ];
+
+    const er = await EmployeeModel.find({});
+    if (er.length > 0) {
+        console.log(er)
+    } else {
+        const jsp = await EmployeeModel.create(details);
+        console.log(jsp);
+    }
+
 
     return NextResponse.json({
         ...data
---
+++ src/server/models/employeeModel.ts
@@ -0,0 +1,12 @@
+import mongoose, { Model } from "mongoose";
+
+
+const employeeSchema = new mongoose.Schema({
+    "name": String,
+    "empId": { type: Number, unique: true },
+    "designation": String,
+    "salary": String
+})
+
+const EmployeeModel = mongoose.models.employee || mongoose.model('employee', employeeSchema);
+export default EmployeeModel;
